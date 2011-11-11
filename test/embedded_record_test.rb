b6e8393e3dabb306c3368efd2ac4390a93d27cce
Wojciech Mach <wojtek@wojtekmach.pl>
2011-11-11 18:55:53 +0100

    Refactor tests

--- test/embedded_record_test.rb
+++ test/embedded_record_test.rb
@@ -5,83 +5,11 @@ require "embedded_record"
 
 class Color
   include EmbeddedRecord::Record
-
   attribute :name
 
   record :red,   :name => "Red"
   record :green, :name => "Green"
   record :blue,  :name => "Blue"
-end
-
-describe "Object with EmbeddedRecord::Record" do
-  it "#is? returns true if id is the same" do
-    Color.find(:green).is?(:green).must_equal true
-  end
-
-  it "#is? returns false if id is not the same" do
-    Color.find(:green).is?(:red).must_equal false
-  end
-
-  it "#index return record index in record set" do
-    Color.find(:red).index.must_equal 0
-    Color.find(:green).index.must_equal 1
-    Color.find(:blue).index.must_equal 2
-  end
-end
-
-describe "Class with EmbeddedRecord::Record" do
-  it "::attributes returns names of record attributes" do
-    Color.attributes.must_equal [:id, :name]
-  end
-
-  it "::all returns all records" do
-    all = Color.all
-    all.must_be_kind_of Array
-    all.size.must_equal 3
-    all.first.must_be_kind_of Color
-    all.first.name.must_equal "Red"
-  end
-
-  it "::ids returns records ids" do
-    Color.ids.must_equal [:red, :green, :blue]
-  end
-
-  it "::find returns record with specified id" do
-    Color.find(:green).id.must_equal :green
-  end
-
-  it "::find returns nil when no record is found for an id" do
-    Color.find(:bad).must_equal nil
-  end
-
-  it "::first returns first record" do
-    Color.first.id.must_equal :red
-  end
-
-  it "::last returns last record" do
-    Color.last.id.must_equal :blue
-  end
-end
-
-describe "Class with EmbeddedRecord" do
-  before do
-    @cls = Class.new
-    @cls.send :include, EmbeddedRecord
-  end
-
-  it "#embed_record defines <name>, <name>_id, <name>_id= methods" do
-    @cls.embed_record :color
-    @cls.method_defined?(:color).must_equal true
-    @cls.method_defined?(:color_id).must_equal true
-    @cls.method_defined?(:color_id=).must_equal true
-  end
-
-  it "#embed_records defines <name>, <name>_ids, <name>_ids= methods" do
-    @cls.embed_records :colors
-    @cls.method_defined?(:colors).must_equal true
-    @cls.method_defined?(:color_ids).must_equal true
-    @cls.method_defined?(:color_ids=).must_equal true
-  end
 end
 
 class Car
@@ -90,79 +18,10 @@ class Car
   attr_accessor :color_mask
 end
 
-describe "Object embedding record" do
-  before do
-    @car = Car.new
-  end
-
-  it "#<name>_id= sets the index of a record" do
-    @car.color_id = :green
-    @car.color_mask.must_equal Color.find(:green).index
-  end
-
-  it "#<name>_id returns id of a record" do
-    @car.color_mask = Color.find(:blue).index
-    @car.color_id.must_equal :blue
-  end
-
-  it "#<name> returns record" do
-    @car.color_mask = Color.find(:blue).index
-    @car.color.must_equal Color.find(:blue)
-  end
-end
-
 class Shirt
   include EmbeddedRecord
   embed_records :colors
   attr_accessor :colors_mask
-end
-
-describe "Object embedding records" do
-  before do
-    @shirt = Shirt.new
-  end
-
-  it "#<name>_ids= sets the indices of records" do
-    @shirt.color_ids = [:red, :blue]
-    @shirt.colors_mask.must_equal(2**0 + 2**2)
-  end
-
-  it "#<name>_ids returns ids of records" do
-    @shirt.colors_mask = 2**1 + 2**2
-    @shirt.color_ids = [:green, :blue]
-  end
-
-  it "#<name> returns records" do
-    @shirt.color_ids = [:green, :blue]
-    @shirt.colors.must_equal [Color.find(:green), Color.find(:blue)]
-  end
-end
-
-describe EmbeddedRecord do
-  before do
-    @cls = Class.new do
-      include EmbeddedRecord
-    end
-  end
-
-  describe "with embed_record and null record" do
-    before do
-      rec = Class.new do
-        include EmbeddedRecord::Record
-        attribute :name
-        record nil, :name => "Empty"
-      end
-
-      @cls.class_eval do
-        attr_accessor :foo_mask
-        embed_record :foo, :class => rec
-      end
-    end
-
-    it "#<name> when <name>_mask is nil returns null record" do
-      @cls.new.foo.name.must_equal "Empty"
-    end
-  end
 end
 
 describe EmbeddedRecord::Record do
@@ -173,11 +32,201 @@ describe EmbeddedRecord::Record do
     end
   end
 
-  it "#record with nil acts as a null object" do
-    @cls.class_eval do
-      record nil, :name => "Empty"
+  it "::attributes returns names pf attributes" do
+    @cls.attribute :val
+    @cls.attributes.must_equal [:id, :name, :val]
+  end
+
+  it "::all returns array of records" do
+    @cls.record 1
+    @cls.record 2
+
+    all = @cls.all
+    all.must_be_kind_of Array
+    all.size.must_equal 2
+
+    all.each_with_index do |record, idx|
+      record.must_be_kind_of @cls
+      record.id.must_equal idx + 1
     end
-    @cls.null_record.name.must_equal "Empty"
-    @cls.all.must_equal []
+  end
+
+  it "::ids returns array of records ids" do
+    @cls.record :one
+    @cls.record :two
+    @cls.ids.must_equal [:one, :two]
+  end
+
+  describe "#find" do
+    it "returns record with a given id" do
+      @cls.record :one
+      @cls.record :two
+      one = @cls.find(:one)
+      one.must_be_kind_of @cls
+      one.id.must_equal :one
+    end
+
+    it "returns nil when no record is found for a given id" do
+      @cls.record :one
+      @cls.find(:two).must_equal nil
+    end
+  end
+
+  it "::first returns first record" do
+    @cls.first.must_equal nil
+    @cls.record 1
+    @cls.record 2
+    @cls.first.id.must_equal 1
+  end
+
+  it "::last returns last record" do
+    @cls.first.must_equal nil
+    @cls.record 1
+    @cls.record 2
+    @cls.last.id.must_equal 2
+  end
+
+  describe "#is?" do
+    before { @cls.record :good}
+
+    it "returns true for good id" do
+      @cls.find(:good).is?(:good).must_equal true
+    end
+
+    it "returns false for bad id" do
+      @cls.find(:good).is?(:bad).must_equal false
+    end
+  end
+
+  it "#index returns record index in record set" do
+    3.times do |i|
+      @cls.record i
+      @cls.find(i).index.must_equal i
+    end
+  end
+
+  describe "#null_record" do
+    it "returns nil if no null object is set" do
+      @cls.null_record.must_equal nil
+    end
+
+    it "returns null object if it is set" do
+      @cls.record nil, :name => "Empty"
+
+      @cls.null_record.name.must_equal "Empty"
+      @cls.null_record.id.must_equal nil
+      @cls.all.wont_include @cls.null_record
+    end
+  end
+end
+
+describe EmbeddedRecord do
+  before do
+    @cls = Class.new do
+      include EmbeddedRecord
+    end
+  end
+
+  describe "::embed_record" do
+    before do
+      @rec_class = Class.new do
+        include EmbeddedRecord::Record
+      end
+    end
+
+    it "defines <name>, <name>_id, <name>_id= methods" do
+      @cls.embed_record :foo, :class => @rec_class
+      @cls.method_defined?(:foo).must_equal true
+      @cls.method_defined?(:foo_id).must_equal true
+      @cls.method_defined?(:foo_id=).must_equal true
+    end
+
+    it "guesses Class based on the name" do
+      @cls.embed_record :color
+      @cls.method_defined?(:color_id).must_equal true
+    end
+  end
+
+  describe "with embedded record" do
+    before do
+      @car = Car.new
+    end
+
+    it "#<name>_id= sets the record mask" do
+      @car.color_id = :blue
+      @car.color_mask.must_equal 2
+    end
+
+    it "#<name>_id returns id of a record" do
+      @car.color_mask = 2
+      @car.color_id.must_equal :blue
+    end
+
+    it "#<name> returns record" do
+      @car.color_mask = 2
+      @car.color.id.must_equal :blue
+    end
+
+    it "#<name> returns nil when no record is set" do
+      @car.color.must_equal nil
+    end
+
+    describe "with null record" do
+      it "#<name> returns null record when no record is set" do
+        rec_class = Class.new(Color) do
+          attribute :name
+          record nil, :name => "Empty"
+        end
+
+        @cls.class_eval do
+          attr_accessor :foo_mask
+          embed_record :foo, :class => rec_class
+        end
+
+        @cls.new.foo.name.must_equal "Empty"
+      end
+    end
+  end
+
+  describe "::embed_records" do
+    before do
+      @rec_class = Class.new do
+        include EmbeddedRecord::Record
+      end
+    end
+
+    it "defines <plural>, <singular>_ids, <singular>_ids= methods" do
+      @cls.embed_records :foos, :class => @rec_class
+
+      @cls.method_defined?(:foos).must_equal true
+      @cls.method_defined?(:foo_ids).must_equal true
+      @cls.method_defined?(:foo_ids=).must_equal true
+    end
+
+    it "guesses Class based on the name" do
+      @cls.embed_records :colors
+      @cls.method_defined?(:color_ids).must_equal true
+    end
+  end
+
+  describe "with embedded records" do
+    before do
+      @obj = Shirt.new
+    end
+
+    it "#<name>_ids= sets the mask" do
+      @obj.color_ids = [:red, :blue]
+      @obj.colors_mask.must_equal(2**0 + 2**2)
+    end
+
+    it "#<name>_ids returns ids of records" do
+      @obj.colors_mask = 2**1 + 2**2
+      @obj.color_ids = [:green, :blue]
+    end
+
+    it "#<name> returns records" do
+      @obj.color_ids = [:green, :blue]
+      @obj.colors.map(&:id).must_equal [:green, :blue]
+    end
   end
 end
